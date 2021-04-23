cb7c96afdc4ab05402a889f454fa36c78d74abd6
dev-oswld <user.oswld@gmail.com>
2021-04-23 00:39:47 -0500

    intro + console app

---
+++ hello_world_10_minutes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_world_10_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
