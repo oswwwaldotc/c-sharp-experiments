79e1fab61efc429086971d602f680517102f6ad9
dev-oswld <user.oswld@gmail.com>
2022-09-10 17:36:30 -0500

    attempt -1

---
+++ csharp_project/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10 * 5;
+            Console.WriteLine(numero/2);
+           
+            short short_number = 140;
+            float n_float = 123.5f;
+
+            char c = 'e';
+            bool state_ = true;
+
+            //Placeholder
+            Console.WriteLine("value: {0} value: {1} value float {2}", numero, short_number, n_float);
+
+            /*
+             * Go to a new repository []
+             * the main reason is the differences to create a project from commands versus on the IDE Visual Studio.
+             * 
+
+        }
+    }
+}
