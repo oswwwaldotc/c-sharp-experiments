2e25377aa22317c925bde5d76a820f980e420dbd
dev-oswld <user.oswld@gmail.com>
2022-09-12 16:41:15 -0500

    section 3

--- csharp_project/Program.cs
+++ csharp_project/Program.cs
@@ -1,27 +1,64 @@
-﻿using System;
-
-namespace MyProject
+﻿namespace MyProject
 {
     class Program
     {
+        private const string Format = "{0} x {1} = {2}";
+
         static void Main(string[] args)
         {
-            int numero = 10 * 5;
-            Console.WriteLine(numero/2);
-           
+            //int numero = 10 * 5;
+            //string numero = Console.ReadLine();
+            Console.WriteLine("Do While - exercise");
+            int numero = int.Parse(Console.ReadLine());
+
+            /*int iterator = 1;
+            int k = 1;
+            do
+            {
+                Console.WriteLine("{0} x {1} = {2}", numero, k, numero * iterator);
+                iterator++;
+                k++;
+            } while (k >= 10);*/
+
+            Console.WriteLine("\nFor");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(Format, numero, i, numero * i);
+            }
+
             short short_number = 140;
             float n_float = 123.5f;
 
             char c = 'e';
-            bool state_ = true;
+            string cc = "ee";
+            bool state_ = false;
 
             //Placeholder
-            Console.WriteLine("value: {0} value: {1} value float: {2} value bool: {3} ", numero, short_number, n_float, state_);
+            Console.WriteLine("\nvalue: {0} value: {1} value float: {2} value bool: {3} ", numero, short_number, n_float, state_);
 
             Console.WriteLine(c);
 
             // Continue this
+            if (state_ == true)
+            {
+                Console.WriteLine(state_);
+            }
+            else if (state_)
+            {
+                Console.WriteLine("!= {0} ", state_);
+            }
 
+            //only with strings?
+            switch (cc)
+            {
+                case "ee":
+                    Console.WriteLine("Is normal");
+                    break;
+
+                default:
+                    Console.WriteLine("breakpoint in switch");
+                    break;
+            }
         }
     }
 }
