861ac5f2abe31e7deb0d03e8d34b38d7489b12bb
dev-oswld <user.oswld@gmail.com>
2022-10-20 21:59:30 -0500

    Example Access Modifiers

---
+++ AccessModifiers/OneMoreClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Si dos clases tienen el mismo nombre pero diferente Namespace es posible usarlas en el mismo proyecto.
+namespace accessmodifiers_00
+{
+    class OneMoreClass
+    {
+        //OneMoreClass moreClass = new OneMoreClass();
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method A01\n");
+
+            //--------------------------------
+            //at accessmodifiers_00.OneMoreClass..ctor()
+            //--------------------------------
+            //at accessmodifiers.Program.Main(System.String[])
+            //moreClass.PrivateMethod();
+        }
+
+        //Usado en esta clase o cualquier otra que herede
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("Protected Method B02\n");
+            //moreClass.ProtectedMethod();
+        }
+
+        //Puede ser visto en cualquier clase del mismo assembly
+        internal void InternalMethod()
+        {
+            Console.WriteLine("Internal Method C03");
+        }
+
+        //Solo visto desde esta clase
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method D04");
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿using accessmodifiers_00;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accessmodifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Con diferente namespace
+            OneMoreClass one = new();
+
+            one.PublicMethod();
+            one.InternalMethod();
+        }
+    }
+}
