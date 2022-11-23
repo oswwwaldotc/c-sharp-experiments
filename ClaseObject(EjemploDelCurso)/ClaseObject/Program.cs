ca9d14b2e6cb3d80e8994d80cbc34f26c0ca0685
dev-oswld <user.oswld@gmail.com>
2022-11-23 12:46:07 -0600

    Example :: Object Class

---
+++ ClaseObject(EjemploDelCurso)/ClaseObject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("John Doe", 31);
+            Persona p2 = new Persona("John Doe", 31);
+            Persona p3 = p1;
+
+            // Comparación por referencia
+            Console.Write("Referencias p1 y p2 son iguales: ");
+            Console.WriteLine(p1 == p2);
+            Console.Write("Referencias p2 y p3 son iguales: ");
+            Console.WriteLine(p2 == p3);
+            Console.Write("Referencias p1 y p3 son iguales: ");
+            Console.WriteLine(p1 == p3);
+
+            Console.WriteLine();
+            // Comparación de objetos
+            Console.Write("Objetos de p1 y p2 son iguales: ");
+            Console.WriteLine(p1.Equals(p2));
+            Console.Write("Objetos de p2 y p3 son iguales: ");
+            Console.WriteLine(p2.Equals(p3));
+            Console.Write("Objetos de p1 y p3 son iguales: ");
+            Console.WriteLine(p1.Equals(p3));
+
+        }
+    }
+}
